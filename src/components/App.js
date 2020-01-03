7db0fadb3546791c8a0b66038ba9b274971363e9
xaviguasch <xaviguasch@gmail.com>
2020-01-03 10:42:53 +0100

    corrected previous starter files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react'
-import ToDoItem from './ToDoItem'
-import InputArea from './InputArea'
 
 function App() {
     const [inputText, setInputText] = useState('')
@@ -18,29 +16,25 @@ function App() {
         setInputText('')
     }
 
-    function deleteItem(id) {
-        setItems(prevItems => {
-            return prevItems.filter((item, index) => {
-                return index !== id
-            })
-        })
-    }
-
     return (
         <div className='container'>
             <div className='heading'>
                 <h1>To-Do List</h1>
             </div>
-            <InputArea />
+            <div className='form'>
+                <input
+                    onChange={handleChange}
+                    type='text'
+                    value={inputText}
+                />
+                <button onClick={addItem}>
+                    <span>Add</span>
+                </button>
+            </div>
             <div>
                 <ul>
-                    {items.map((todoItem, index) => (
-                        <ToDoItem
-                            key={index}
-                            id={index}
-                            text={todoItem}
-                            onChecked={deleteItem}
-                        />
+                    {items.map(todoItem => (
+                        <li>{todoItem}</li>
                     ))}
                 </ul>
             </div>
